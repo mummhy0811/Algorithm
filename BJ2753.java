5a94831e22455b355c526d0bc9015dcf42c0a582
mummhy0811 <76941500+mummhy0811@users.noreply.github.com>
2021-06-17 00:25:58 +0900

    Create BJ2753.java

---
+++ BJ2753.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		Scanner scanner=new Scanner(System.in);
+		int A=scanner.nextInt(); 
+		if((A%4==0)&&(A%100!=0||A%400==0)) 
+			System.out.println("1");
+		else System.out.println("0");
+		scanner.close();
+	}
+}
