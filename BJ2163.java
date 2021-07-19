e9092a47a530f36c86efc3be38dc382e73e7c3e3
mummhy0811 <76941500+mummhy0811@users.noreply.github.com>
2021-07-19 23:52:10 +0900

    Create BJ2163.java

---
+++ BJ2163.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner=new Scanner(System.in);
+		int n=scanner.nextInt();
+		int m=scanner.nextInt();
+		int ans=n*m-1;
+		System.out.println(ans);
+		scanner.close();
+		
+	}
+}
