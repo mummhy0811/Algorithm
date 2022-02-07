f838debf7f41d48fbdff50f4929313a87377dcec
mummhy0811 <mummhy0811@inu.ac.kr>
2022-02-08 00:06:39 +0900

    refactor: 백준 1181번 코드 간결하게 수정

--- BJ1181.java
+++ BJ1181.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.Arrays;
 import java.util.TreeSet;
 public class BJ1181 {
 	public static void main(String[] args) throws NumberFormatException, IOException {
@@ -12,15 +13,7 @@ public class BJ1181 {
 			set.add(word); //중복 제거 및 오름차순 정렬
 		}
 		String arr[]=set.toArray(new String[set.size()]); //set을 배열로 변경
-		for(int i=0;i<arr.length-1;i++) {
-			for(int j=0;j<arr.length-i-1;j++) {
-				if(arr[j].length()>arr[j+1].length()) {
-					tmp=arr[j];
-					arr[j]=arr[j+1];
-					arr[j+1]=tmp;
-				}
-			}
-		}
+		Arrays.sort(arr, (x, y)->x.length()-y.length());
 		for(int i=0;i<arr.length;i++) bw.write(arr[i]+"\n");
 		br.close(); bw.flush(); bw.close();
 	}
