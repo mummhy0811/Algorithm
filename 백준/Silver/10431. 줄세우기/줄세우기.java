bed1dadad9e592835ab332e24c563169e5726edc
mummhy0811 <76941500+mummhy0811@users.noreply.github.com>
2023-11-28 16:13:17 +0900

    [Silver V] Title: 줄세우기, Time: 312 ms, Memory: 22700 KB -BaekjoonHub

--- 백준/Silver/10431. 줄세우기/줄세우기.java
+++ 백준/Silver/10431. 줄세우기/줄세우기.java
@@ -10,26 +10,20 @@ class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int n = Integer.parseInt(br.readLine());
-        int[][] arr = new int[n][21];
-        for(int i=0;i<n;i++){
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            arr[i][0]=Integer.parseInt(st.nextToken());
-            for(int j=1;j<21;j++){
-                arr[i][j]=Integer.parseInt(st.nextToken());
-            }
-        }
 
         for(int i=0;i<n;i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int t = Integer.parseInt(st.nextToken());
             List<Integer> sort= new ArrayList<>();
             int num=0;
-            sort.add(arr[i][1]);
-           for(int j=2;j<21;j++){
-               int height = arr[i][j];
-               long p =  sort.stream().filter(s -> s>height).count();
-               num+=p;
-               sort.add((int) (j-p-1), height);
-           }
-           bw.write(arr[i][0]+" "+num+"\n");
+            sort.add(Integer.parseInt(st.nextToken()));
+            for(int j=2;j<21;j++){
+                int height = Integer.parseInt(st.nextToken());
+                long p =  sort.stream().filter(s -> s>height).count();
+                num+=p;
+                sort.add((int) (j-p-1), height);
+            }
+            bw.write(t+" "+num+"\n");
         }
         bw.flush();
         bw.close();
