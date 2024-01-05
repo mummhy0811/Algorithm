07e2d77c1413ea11507d515bb5a561b47f7b3a5a
mummhy0811 <76941500+mummhy0811@users.noreply.github.com>
2024-01-05 17:45:15 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 168 ms, Memory: 18456 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
@@ -7,10 +7,8 @@ import java.util.StringTokenizer;
 
 public class Main {
     public static int n;
-    public static long max, min;
-    public static String[] oper;
-    public static int[] arr;
-    public static boolean[] used;
+    public static int max, min;
+    public static int[] arr, oper;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -18,27 +16,17 @@ public class Main {
 
         n = Integer.parseInt(br.readLine());
         arr = new int[n];
-        oper = new String[n-1];
-        used = new boolean[n-1];
+        oper = new int[4];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         for(int i=0;i<n;i++) arr[i] = Integer.parseInt(st.nextToken());
+
         st = new StringTokenizer(br.readLine());
 
-        int t=0;
+        for(int i=0;i<4;i++) oper[i] = Integer.parseInt(st.nextToken());
 
-        for(int i=0;i<4;i++) {
-            int n = Integer.parseInt(st.nextToken());
-            for(int j=0;j<n;j++) {
-                if(i==0) oper[t++]="+";
-                else if(i==1) oper[t++]="-";
-                else if(i==2) oper[t++]="*";
-                else oper[t++]="/";
-            }
-        }
-
-        max=Long.MIN_VALUE;
-        min=Long.MAX_VALUE;
+        max=Integer.MIN_VALUE;
+        min=Integer.MAX_VALUE;
 
         search(1, arr[0]);
         bw.write(max+"\n");
@@ -46,23 +34,22 @@ public class Main {
         bw.flush();
         bw.close();
     }
-    private static void search(int depth, long ans){
+    private static void search(int depth, int ans){
         if(depth==n){
             max=Math.max(max, ans);
             min=Math.min(min, ans);
             return;
         }
-
-        for(int i=0;i<n-1;i++){
-            if(!used[i]){
-                used[i]=true;
-                switch ( oper[i]) {
-                    case "+":  search(depth+1, ans+arr[depth]); break;
-                    case "-": search(depth+1, ans-arr[depth]); break;
-                    case "*": search(depth+1, ans*arr[depth]);  break;
-                    case "/": search(depth+1, ans/arr[depth]); break;
+        for(int i=0;i<4;i++){
+            if(oper[i]>0){
+                oper[i]--;
+                switch ( i) {
+                    case 0:  search(depth+1, ans+arr[depth]); break;
+                    case 1: search(depth+1, ans-arr[depth]); break;
+                    case 2: search(depth+1, ans*arr[depth]);  break;
+                    case 3: search(depth+1, ans/arr[depth]); break;
                 }
-                used[i]=false;
+                oper[i]++;
             }
         }
     }
