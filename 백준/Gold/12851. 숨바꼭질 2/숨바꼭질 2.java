7d658947283ebdc68349bd8f6bba1f0c4aec0c51
Hyeyoung Moon <76941500+mummhy0811@users.noreply.github.com>
2024-03-27 23:18:05 +0900

    [Gold IV] Title: 숨바꼭질 2, Time: 212 ms, Memory: 22860 KB -BaekjoonHub

--- 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.java
+++ 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.java
@@ -1,58 +1,52 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
-class Main {
-    static int N, K;
-    static int[] time = new int[100001];
-    static int minTime = 987654321;
-    static int count = 0;
-
-    public static void main(String[] args) throws Exception {
+public class Main {
+    static int n, k;
+    static int minTime=100001, cnt = 0;
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
 
-        st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
-
-        if (N >= K) {
-            System.out.println((N-K) + "\n1");
+        if (n >= k) {
+            System.out.println((n-k) + "\n1");
             return;
         }
-
         bfs();
-
-        System.out.println(minTime + "\n" + count);
+        System.out.println(minTime + "\n" + cnt);
     }
-
-    static void bfs() {
-        Queue<Integer> q = new LinkedList<Integer>();
-
-        q.add(N);
-        time[N] = 1;
+    static void bfs(){
+        Queue<Integer> q = new LinkedList<>();
+        int[] depth = new int[100001];
+        q.add(n);
+        depth[n]=1;
 
         while (!q.isEmpty()) {
-            int now = q.poll();
+            int num = q.poll();
 
-            if (minTime < time[now]) return;
+            if(minTime<depth[num]) return;
 
-            for (int i=0; i<3; i++) {
+            for(int i=0;i<3;i++){
                 int next;
+                if (i == 0)
+                    next = num + 1;
+                else if (i == 1)
+                    next = num - 1;
+                else
+                    next = num * 2;
 
-                if (i == 0)         next = now + 1;
-                else if (i == 1)    next = now - 1;
-                else                next = now * 2;
+                if(next<0 || next>100000) continue;
 
-                if (next < 0 || next > 100000) continue;
-
-                if (next == K) {
-                    minTime = time[now];
-                    count++;
+                if(next==k){
+                    minTime=depth[num];
+                    cnt++;
                 }
 
-                if (time[next] == 0 || time[next] == time[now] + 1) {
+                if(depth[next]==0 || depth[next]==depth[num]+1){
                     q.add(next);
-                    time[next] = time[now] + 1;
+                    depth[next]=depth[num]+1;
                 }
             }
         }
