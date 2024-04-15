f3ef651243fb21945cc0d4768757fbd96b4de629
Hyeyoung Moon <76941500+mummhy0811@users.noreply.github.com>
2024-04-15 21:13:06 +0900

    [Gold V] Title: 토마토, Time: 760 ms, Memory: 143516 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -14,8 +14,8 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        m = Integer.parseInt(st.nextToken()); //가로
-        n = Integer.parseInt(st.nextToken()); //세로
+        m = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
         arr = new int[n][m][2];
 
         green=0;
@@ -25,10 +25,12 @@ public class Main {
                 arr[i][j][0]=Integer.parseInt(st.nextToken());
                 if(arr[i][j][0]==0) {
                     green++;
+                }else if(arr[i][j][0]==1){
+                    q.add(new int[]{i, j});
                 }
             }
         }
-        //저장될 때부터 모든 토마토가 익어있는 상태
+
         if(green==0) {
             bw.write("0");
             bw.flush();
@@ -36,24 +38,19 @@ public class Main {
             return;
         }
 
-        int days=0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(arr[i][j][0]==1 && arr[i][j][1]==0) {
-                    q.add(new int[]{i, j});
-                }
-            }
-        }
-
         makeRed();
 
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                days=Math.max(days,arr[i][j][1] );
+        if(green>0)
+            bw.write("-1");
+        else{
+            int days=0;
+            for(int i=0;i<n;i++){
+                for(int j=0;j<m;j++){
+                    days=Math.max(days,arr[i][j][1] );
+                }
             }
+            bw.write(days+"");
         }
-        if(green>0) bw.write("-1");
-        else bw.write(days+"");
         bw.flush();
         bw.close();
     }
@@ -62,15 +59,14 @@ public class Main {
             int[] now = q.poll();
             int nowR=now[0];
             int nowC=now[1];
-//            System.out.println(nowR+" "+nowC+"방문, "+arr[nowR][nowC][1]);
 
             for(int i=0;i<4;i++){
                 int nextR=nowR+x[i];
                 int nextC=nowC+y[i];
-                if(nextR<n && nextC<m && nextR>=0 && nextC>=0 && arr[nextR][nextC][0]!=-1){
+                if(nextR<n && nextC<m && nextR>=0 && nextC>=0){
                     if(arr[nextR][nextC][0]==0 && arr[nextR][nextC][1]==0){
                         green--;
-                        q.add(new int[]{nextR, nextC, arr[nowR][nowC][1]+1});
+                        q.add(new int[]{nextR, nextC});
                         arr[nextR][nextC][1]=arr[nowR][nowC][1]+1;
                     }
                 }
