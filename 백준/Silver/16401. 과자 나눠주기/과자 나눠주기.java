d5c6913cfc15f7ea01ed316cce8002b8dbed5370
Hyeyoung Moon <76941500+mummhy0811@users.noreply.github.com>
2024-04-16 14:24:03 +0900

    [Silver II] Title: 과자 나눠주기, Time: 1304 ms, Memory: 126240 KB -BaekjoonHub

--- 백준/Silver/16401. 과자 나눠주기/과자 나눠주기.java
+++ 백준/Silver/16401. 과자 나눠주기/과자 나눠주기.java
@@ -3,14 +3,15 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
+    static int m, n;
+    static int[] arr;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int m = Integer.parseInt(st.nextToken()); //조카
-        int n = Integer.parseInt(st.nextToken()); //과자
-
-        int[] arr = new int[n];//과자의 길이
+        m = Integer.parseInt(st.nextToken()); //조카
+        n = Integer.parseInt(st.nextToken()); //과자
+        arr = new int[n];//과자의 길이
 
         st = new StringTokenizer(br.readLine());
         for(int i=0;i<n;i++) arr[i]=Integer.parseInt(st.nextToken());
@@ -20,11 +21,7 @@ public class Main {
 
         while(l<=r){
             int mid=(l+r)/2;
-            int given=0;
-            for(int i=0;i<n;i++){
-                if(arr[i]>=mid) given=given+arr[i]/mid;
-            }
-            if(given<m){
+            if(calcGiven(mid)<m){
                 r=mid-1;
             }else{
                 l=mid+1;
@@ -33,5 +30,12 @@ public class Main {
         bw.write(r+"");
         bw.flush();
         bw.close();
+    }
+    static int calcGiven(int mid){
+        int given=0;
+        for(int i=0;i<n;i++){
+            if(arr[i]>=mid) given=given+arr[i]/mid;
+        }
+        return given;
     }
 }
