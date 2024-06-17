8df948b54cdb6855426c6171f1c7c0cdb1cae85b
Hyeyoung Moon <76941500+mummhy0811@users.noreply.github.com>
2024-06-17 22:05:05 +0900

    [Silver II] Title: 좌표 압축, Time: 3048 ms, Memory: 264052 KB -BaekjoonHub

---
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.Map;
+import java.util.StringTokenizer;
+import java.util.TreeMap;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        Map<Integer, Integer> map = new TreeMap<>();
+        int[] arr = new int[n];
+
+        for(int i=0;i<n;i++){
+            int num=Integer.parseInt(st.nextToken());
+            if(map.containsKey(num)){
+                map.put(num, map.get(num)+1);
+            }else{
+                map.put(num, 1);
+            }
+            arr[i]=num;
+        }
+
+        int idx=0;
+        for(Integer num:map.keySet()){
+            map.put(num, idx);
+            idx++;
+        }
+        
+        for(int i=0;i<n;i++){
+            bw.write(map.get(arr[i])+" ");
+        }
+        bw.flush();
+        bw.close();
+    }
+}
