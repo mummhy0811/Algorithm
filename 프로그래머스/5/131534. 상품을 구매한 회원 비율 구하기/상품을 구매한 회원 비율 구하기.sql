f0b157622db9cbc0b92af3d21b183e369d2bce5b
Hyeyoung Moon <76941500+mummhy0811@users.noreply.github.com>
2024-07-05 22:49:56 +0900

    [level 5] Title: 상품을 구매한 회원 비율 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/5/131534. 상품을 구매한 회원 비율 구하기/상품을 구매한 회원 비율 구하기.sql
+++ 프로그래머스/5/131534. 상품을 구매한 회원 비율 구하기/상품을 구매한 회원 비율 구하기.sql
@@ -1,13 +1,11 @@
-set @user_cnt = (
-    select count(*) from USER_INFO where YEAR(JOINED)='2021'
-);
-
-SELECT YEAR(SALES_DATE) YEAR,  MONTH(SALES_DATE) MONTH, count(distinct O.user_id ) PURCHASED_USERS,  ROUND(count(distinct O.user_id )/@user_cnt, 1) PUCHASED_RATIO
-FROM ONLINE_SALE O
-inner join(
-    select USER_ID
-    from USER_INFO
-    where YEAR(JOINED)='2021'
-) U on O.USER_ID=U.USER_ID
-GROUP BY YEAR(O.SALES_DATE) , MONTH(O.SALES_DATE)
-ORDER BY YEAR(O.SALES_DATE) , MONTH(O.SALES_DATE);
+SELECT 
+    YEAR(SALES_DATE) YEAR,  
+    MONTH(SALES_DATE) MONTH, 
+    count(distinct O.user_id ) PURCHASED_USERS,  
+    ROUND(count(distinct O.user_id )/(select count(*) from USER_INFO where YEAR(JOINED)=2021), 1) PUCHASED_RATIO
+FROM 
+    ONLINE_SALE O
+INNER JOIN USER_INFO U
+ON O.USER_ID=U.USER_ID AND YEAR(U.JOINED)=2021
+GROUP BY YEAR , MONTH
+ORDER BY YEAR , MONTH;
