72cb2da3d68ee9725b8aedfab0d18dcfb05837a0
Hyeyoung Moon <76941500+mummhy0811@users.noreply.github.com>
2024-08-04 18:03:12 +0900

    [Silver I] Title: 회전 초밥, Time: 2480 ms, Memory: 300684 KB -BaekjoonHub

---
+++ 백준/Silver/2531. 회전 초밥/회전 초밥.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int d = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+
+        List<Integer> belt = new ArrayList<>();
+        for(int i=0;i<n;i++){
+            belt.add(Integer.parseInt(br.readLine()));
+        }
+        List<Integer> now = new ArrayList<>();
+        for(int i=0;i<k;i++){
+            int init = belt.get(i);
+            now.add(init);
+            belt.add(belt.get(i));
+        }
+
+        int max=1;
+        Set<Integer> maxSet = new HashSet<>();
+        for(int i = 0; i<n; i++){
+            Set<Integer>set = new HashSet<>(now);
+            if(max<set.size()){
+                max= set.size();
+                maxSet=Set.copyOf(set);
+            }else if(max==set.size() && !maxSet.equals(set) && !now.contains(c)){
+                maxSet=Set.copyOf(set);
+                max= set.size();
+            }
+            now.remove(belt.get(i));
+            now.add(belt.get(i+k));
+        }
+        if(!maxSet.contains(c)) bw.write((maxSet.size()+1)+"");
+        else bw.write(maxSet.size()+"");
+        bw.flush();
+        bw.close();
+    }
+}
