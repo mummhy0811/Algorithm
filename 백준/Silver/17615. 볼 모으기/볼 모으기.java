c4ef56a5de9380aef0be133332516b0d34b34f00
hailey <76941500+mummhy0811@users.noreply.github.com>
2024-10-15 23:04:57 +0900

    [Silver I] Title: 볼 모으기, Time: 216 ms, Memory: 19692 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/17615. 볼 모으기/볼 모으기.java
+++ 백준/Silver/17615. 볼 모으기/볼 모으기.java
@@ -13,44 +13,43 @@ public class Main {
         bw.close();
     }
     public static int run(int N, String S){
-
+        char[] arr= S.toCharArray();
         int R = 0, B = 0, min = 500000;
         for(int i=0;i<S.length();i++){
-            if(S.charAt(i)=='R') R++;
-            else if(S.charAt(i)=='B' ) B++;
+            if(arr[i]=='R') R++;
+            else if(arr[i]=='B' ) B++;
         }
-        int move = R;
+        int move = 0;
         //R을 앞으로
-        for(int i=0;i<N;i++){
-            if(S.charAt(i)=='R') move--;
+        for(int i=0;i<S.length();i++){
+            if(arr[i]=='R') move++;
             else break;
         }
-        min = Math.min(min, move);
+        min = Math.min(min, R-move);
 
-        move = R;
+        move = 0;
         //R을 뒤
-        for(int i=N-1;i>0;i--){
-            if(S.charAt(i)=='R') move--;
+        for(int i=N-1;i>=0;i--){
+            if(arr[i]=='R') move++;
             else break;
         }
-        min = Math.min(min, move);
+        min = Math.min(min, R-move);
 
-        move = B;
+        move = 0;
         //B를 앞으로
-        for(int i=0;i<N;i++){
-            if(S.charAt(i)=='B') move--;
+        for(int i=0;i<S.length();i++){
+            if(arr[i]=='B') move++;
             else break;
         }
-        min = Math.min(min, move);
+        min = Math.min(min,B- move);
 
-        move = B;
+        move = 0;
         //B를 뒤
-        for(int i=N-1;i>0;i--){
-            if(S.charAt(i)==B) move--;
+        for(int i=N-1;i>=0;i--){
+            if(arr[i]=='B') move++;
             else break;
         }
-        min = Math.min(min, move);
+        min = Math.min(min, B-move);
         return min;
     }
-
 }
