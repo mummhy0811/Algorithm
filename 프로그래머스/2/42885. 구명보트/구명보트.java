dbcce96d12216d51d9cf41c6e45e258ecd89859b
hailey <76941500+mummhy0811@users.noreply.github.com>
2025-01-08 16:07:06 +0900

    [level 2] Title: 구명보트, Time: 11.07 ms, Memory: 57.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        Arrays.sort(people);
+        
+        int l = 0;
+        int r = people.length-1;
+        
+        while(l<=r){
+            if(people[l]+people[r]<=limit){
+                l++;
+            }
+            answer++;
+            r--;
+        }
+
+        return answer;
+    }
+}
