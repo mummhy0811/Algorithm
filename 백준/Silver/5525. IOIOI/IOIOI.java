fd5b66566b18c8a467f95547e3055554fcfb4858
hailey <76941500+mummhy0811@users.noreply.github.com>
2025-01-14 16:36:25 +0900

    [Silver I] Title: IOIOI, Time: 244 ms, Memory: 20112 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.java
+++ 백준/Silver/5525. IOIOI/IOIOI.java
@@ -1,29 +1,26 @@
 import java.io.*;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int n = Integer.parseInt(br.readLine());
-        int m = Integer.parseInt(br.readLine());
-        String s = br.readLine();
-        StringBuilder sb = new StringBuilder();
-        sb.append('I');
-        sb.append("OI".repeat(Math.max(0, n)));
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+        String S = br.readLine();
 
-        int len=sb.length();
-
-        int ans=0;
-        for(int i=0;i<=m-len;i++){
-            if(s.charAt(i)=='I'){
-                if(s.substring(i, i+len).equals(sb.toString())){
-                    ans++;
+        int cnt = 0;
+        int pattern = 0;
+        for(int i=1;i<M-1;i++){
+            if(S.charAt(i-1)=='I' && S.charAt(i)=='O' &&S.charAt(i+1)=='I'){
+                pattern++;
+                if (pattern == N) {
+                    cnt++;
+                    pattern --;
                 }
+                i++;
+            }else{
+                pattern = 0;
             }
         }
-        bw.write(ans+"");
-        bw.flush();
-        bw.close();
+        System.out.println(cnt);
     }
 }
