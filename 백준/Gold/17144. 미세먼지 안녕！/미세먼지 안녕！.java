bb545e157b4111c13b9869fe0e8515fa8061e418
hailey <76941500+mummhy0811@users.noreply.github.com>
2025-01-23 16:55:35 +0900

    [Gold IV] Title: 미세먼지 안녕!, Time: 496 ms, Memory: 41760 KB -BaekjoonHub

--- 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.java
+++ 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.java
@@ -11,7 +11,6 @@ public class Main {
         int c = Integer.parseInt(st.nextToken());
         int t = Integer.parseInt(st.nextToken());
         int[][] arr = new int[r][c];
-        int[][] blow;
 
         int top = 0, bottom = 0;
         for(int i=0;i<r;i++){
@@ -27,22 +26,24 @@ public class Main {
         }
 
         int[] mx = {0, 1, 0, -1}, my={1, 0, -1, 0};
-        int answer = 0;
+
         while(t-->0){
-            blow = new int[r][c];
+            int[][] blow = new int[r][c];
             for(int i=0;i<r;i++){
                 for(int j=0;j<c;j++){
-                    if(arr[i][j]==0 || arr[i][j]==-1) continue;
-                    int temp = (int) Math.floor(arr[i][j]/5);
+                    if(arr[i][j]>0){
+                        int temp = arr[i][j]/5;
 
-                    for(int k=0;k<4;k++){
-                        int nx = i+mx[k];
-                        int ny = j+my[k];
+                        for(int k=0;k<4;k++){
+                            int nx = i+mx[k];
+                            int ny = j+my[k];
 
-                        if(nx<0 || ny<0 || nx>=r || ny>=c || arr[nx][ny]==-1) continue;
-                        blow[nx][ny]+=temp;
-                        blow[i][j]-=temp;
+                            if(nx<0 || ny<0 || nx>=r || ny>=c || arr[nx][ny]==-1) continue;
+                            blow[nx][ny]+=temp;
+                            blow[i][j]-=temp;
+                        }
                     }
+
                 }
             }
             for(int i=0;i<r;i++){
@@ -51,57 +52,25 @@ public class Main {
                 }
             }
 
-            int topBefore = arr[top][1];
-            int bottomBefore = arr[bottom][1];
+            //반시계
+            for(int i = top-1 ;i>0;i--) arr[i][0] = arr[i-1][0]; //밑
+            for(int i = 0;i<c-1;i++) arr[0][i] = arr[0][i+1]; //왼
+            for(int i = 0;i<top;i++) arr[i][c-1] = arr[i+1][c-1]; //위
+            for(int i = c-1; i>1;i--) arr[top][i] = arr[top][i-1]; //오
             arr[top][1] = 0;
+
+            //시계
+            for(int i = bottom+1;i<r-1;i++) arr[i][0] = arr[i+1][0]; //위
+            for(int i = 0;i<c-1;i++) arr[r-1][i] = arr[r-1][i+1];//왼
+            for(int i=r-1;i>bottom;i--) arr[i][c-1] = arr[i-1][c-1];//밑
+            for(int i = c-1; i>1;i--) arr[bottom][i] = arr[bottom][i-1]; //오
             arr[bottom][1] = 0;
-            for(int i=2;i<c;i++){
-                int tmp = arr[top][i];
-                arr[top][i] = topBefore;
-                topBefore = tmp;
-
-                int tmp2 = arr[bottom][i];
-                arr[bottom][i] = bottomBefore;
-                bottomBefore = tmp2;
-            }
-
-            for(int i=top-1;i>=0;i--){
-                int tmp = arr[i][c-1];
-                arr[i][c-1] = topBefore;
-                topBefore = tmp;
-            }
-
-            for(int i=bottom+1;i<r;i++){
-                int tmp = arr[i][c-1];
-                arr[i][c-1] = bottomBefore;
-                bottomBefore = tmp;
-            }
-
-            for(int i=c-2;i>0;i--){
-                int tmp = arr[0][i];
-                arr[0][i] = topBefore;
-                topBefore = tmp;
-
-                int tmp2 = arr[r-1][i];
-                arr[r-1][i] = bottomBefore;
-                bottomBefore = tmp2;
-            }
-
-            for(int i=0;i<top;i++){
-                int tmp = arr[i][0];
-                arr[i][0] = topBefore;
-                topBefore = tmp;
-            }
-            for(int i=r-1;i>bottom;i--){
-                int tmp = arr[i][0];
-                arr[i][0] = bottomBefore;
-                bottomBefore = tmp;
-            }
         }
 
+        int answer = 0;
         for(int i=0;i<r;i++){
-            for(int j=0;j<c;j++){
-                answer+=arr[i][j];
+            for(int j=0;j<c;j++) {
+                answer += arr[i][j];
             }
         }
         System.out.println(answer+2);
