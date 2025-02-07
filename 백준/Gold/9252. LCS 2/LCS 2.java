672ef1dc928e747540fac8cad02d1506313e6d82
hailey <76941500+mummhy0811@users.noreply.github.com>
2025-02-07 14:35:48 +0900

    [Gold IV] Title: LCS 2, Time: 140 ms, Memory: 18452 KB -BaekjoonHub

--- 백준/Gold/9252. LCS 2/LCS 2.java
+++ 백준/Gold/9252. LCS 2/LCS 2.java
@@ -5,35 +5,48 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        String S1 = br.readLine();
-        String S2 = br.readLine();
+        String s1 = br.readLine();
+        String s2 = br.readLine();
 
-        int[][] cnt = new int[S1.length()+1][S2.length()+1];
+        int len1 = s1.length();
+        int len2 = s2.length();
+        int[][] arr = new int[len1+1][len2+1];
 
-        for(int i=1;i<=S1.length();i++){
-            for(int j=1;j<=S2.length();j++){
-                if(S1.charAt(i-1)==S2.charAt(j-1)) cnt[i][j] = cnt[i-1][j-1]+1;
-                else cnt[i][j] = Math.max(cnt[i][j-1], cnt[i-1][j]);
+        for(int i = 1;i<=len1;i++){
+            char c = s1.charAt(i-1);
+            for(int j=1;j<=len2;j++){
+                if(c == s2.charAt(j-1)) arr[i][j] = arr[i-1][j-1]+1;
+                else arr[i][j] = Math.max(arr[i-1][j], arr[i][j-1]);
             }
         }
-        int len=cnt[S1.length()][S2.length()];
-        int i=S1.length();
-        int j=S2.length();
-        while(len>0){
-            int now = cnt[i][j];
-            if(i-1>0 && cnt[i-1][j]==now){
-                i--;
-            }else if(j-1>0 && cnt[i][j-1]==now){
-                j--;
-            }else{
-                sb.append(S1.charAt(i-1));
-                i--;
-                j--;
-                len--;
-            }
-        }
-        System.out.println(cnt[S1.length()][S2.length()]);
-        if(cnt[S1.length()][S2.length()]!=0)System.out.println(sb.reverse());
 
+        int next = arr[len1][len2];
+        System.out.println(next);
+
+        int i = len1;
+        int j = len2;
+        while(next>0){
+            int now = arr[i][j];
+            while(true){
+                if(arr[i-1][j-1]==now){
+                    i--;
+                    j--;
+                }
+                else if(arr[i][j-1]==now){
+                    j--;
+                }
+                else if(arr[i-1][j]==now){
+                    i--;
+                }
+                else{
+                    i--;
+                    j--;
+                    next--;
+                    break;
+                }
+            }
+            sb.append(s1.charAt(i));
+        }
+        System.out.println(sb.reverse());
     }
 }
